cfd8486fefd75f31d52edd16678a7525680aecbe
Vadym Novoselskyi <novoselskyi.vadim@gmail.com>
2026-02-19 11:03:31 +0100

    rm some shit

--- labb4/constraint-parser.rb
+++ labb4/constraint-parser.rb
@@ -420,95 +420,6 @@ class Parser
 
 end
 
-##############################################################################
-#
-# This part defines the dice language
-#
-##############################################################################
-
-class DiceRoller
-        
-  def DiceRoller.roll(times, sides)
-    # Throw "sides" sided dice "times" times and return the total
-    result = (1..times).inject(0) {|sum, _| sum + rand(sides) + 1 }
-
-    # Need to get a new instance since this is a class method
-    LoggerFactory.get().info("Rolled #{times}d#{sides} getting #{result}")
-
-    return result
-  end
-  
-  def initialize
-
-    @logger = LoggerFactory.get()
-
-    @diceParser = Parser.new("dice roller") do
-      token(/\s+/) # Ignore whitespace
-      token(/\d+/) {|m| m.to_i } # Any string of digits is converted to an Integer
-      token(/./) {|m| m }
-      
-      start :expr do 
-        match(:expr, '+', :term) {|a, _, b| a + b }
-        match(:expr, '-', :term) {|a, _, b| a - b }
-        match(:term)
-      end
-      
-      rule :term do 
-        match(:term, '*', :dice) {|a, _, b| a * b }
-        match(:term, '/', :dice) {|a, _, b| a / b }
-        match(:dice)
-      end
-
-      rule :dice do
-        match(:atom, 'd', :sides) {|a, _, b| DiceRoller.roll(a, b) }
-        match(:atom)
-      end
-      
-      rule :sides do
-        match('%') { 100 }
-        match(:atom)
-      end
-      
-      rule :atom do
-        # Match the result of evaluating an integer expression, which
-        # should be an Integer
-        match(Integer)
-        match('(', :expr, ')') {|_, a, _| a }
-      end
-    end
-  end
-  
-  def done(str)
-    ["quit","exit","bye","done",""].include?(str.chomp)
-  end
-  
-  def roll
-    print "[diceroller] "
-    str = gets
-    if done(str) then
-      puts "Bye."
-    else
-      puts "=> #{@diceParser.parse str}"
-      roll
-    end
-  end
-
-end
-
-# Examples of use
-
-# irb(main):1696:0> DiceRoller.new.roll
-# [diceroller] 1+3
-# => 4
-# [diceroller] 1+1d4
-# => 2
-# [diceroller] 1+1d4
-# => 3
-# [diceroller] (2+8*1d20)*3d6
-# => 306
-
-#DiceRoller.new.roll
-
 # ********** TASK *************** 
 
 # Someone has tried to implement a parser and constraint network
@@ -592,7 +503,7 @@ class ConstraintParser < Parser
     
       rule :atom do
         match(Connector)
-        match('(', :expr, ')'end) {|_, a, _| a }
+        match('(', :expr, ')') {|_, a, _| a }
       end
     end
   end
