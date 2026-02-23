740a5b3b3f9d4450dacc1090a475dbad4d2acf58
Vadym Novoselskyi <novoselskyi.vadim@gmail.com>
2026-02-23 14:34:12 +0100

    add tests for multiple vars

--- labb4/test_constraint_parser.rb
+++ labb4/test_constraint_parser.rb
@@ -34,7 +34,7 @@ class TestConstraintParser < Test::Unit::TestCase
     assert_equal(1, a[0].value)
   end
   
-  def test_double_var() 
+  def test_double_vars() 
     a, b = @parser.parse("a = b")
     a.user_assign(5)
     assert_equal(5, a.value)
@@ -82,5 +82,21 @@ class TestConstraintParser < Test::Unit::TestCase
     f.user_assign(100)
     assert_equal(37, c.value)
     assert_equal(100, f.value)
+  end
+
+  def test_multiple_vars()
+    a, b, c = @parser.parse("a * b = c * 5")
+    b.user_assign(5)
+    c.user_assign(10)
+    assert_equal(10, a.value)
+    assert_equal(5, b.value)
+    assert_equal(10, c.value)
+
+    a, b, c = @parser.parse("a + b = 50 + c")
+    b.user_assign(10)
+    c.user_assign(15)
+    assert_equal(55, a.value)
+    assert_equal(10, b.value)
+    assert_equal(15, c.value)
   end
 end
